655a8e5a6ff8e9f77465156ede8f41a398f71772
adie <2993448939@qq.com>
2022-11-23 15:43:50 +0800

    no message

--- pc-practive/src/views/login.vue
+++ pc-practive/src/views/login.vue
@@ -4,28 +4,26 @@
             <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
                 <img src="../assets/img/logo1.2bee7fa7.png" alt="">
                 <h2>国海投顾系统</h2>
-                <el-form-item prop="id" class="input">
+                <el-form-item prop="username" class="input">
                     <el-input v-model="ruleForm.username" placeholder="请输入账号" clearable></el-input>
                 </el-form-item>
                 <el-form-item prop="password">
-                    <el-input placeholder="请输入密码" v-model="ruleForm.password" clearable></el-input>
+                    <el-input type="password" placeholder="请输入密码" v-model="ruleForm.password" clearable></el-input>
                 </el-form-item>
                 <el-form-item prop="code" class="code">
                     <el-input class="code2" v-model="ruleForm.code" placeholder="请输入图形验证码" clearable></el-input>
                 </el-form-item>
                 <el-form-item class="confirmCode">
-                    <div></div>
+                    <img :src="codeImg" class="codeImg" @click="oNcodeImg()" />
                 </el-form-item>
-                <el-button class="loginBtn" type="primary" @click="$router.push('/loginLog')">登录</el-button>
+                <el-button class="loginBtn" type="primary" @click="login(ruleForm)">登录</el-button>
             </el-form>
         </div>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-
+import Cookies from 'js-cookie' //引用
 export default {
     name: "login",
     components: {
@@ -33,6 +31,7 @@ export default {
     },
     data () {
         return {
+            codeImg:'',
             ruleForm: {
                 username: '',
                 password: '',
@@ -40,9 +39,8 @@ export default {
                 uuid: '',
                 rememberMe: false,
             },
-
             rules: {
-                id: [
+                username: [
                     { required: true, message: '请输入账号', trigger: 'blur' },
                 ],
                 password: [
@@ -58,25 +56,70 @@ export default {
 
 
     },
+    mounted () {
+        this.oNcodeImg();
+    },
     methods: {
-        // login (ruleForm) {
-        //     this.$refs[ruleForm].validate((valid) => {
-        //         if (valid) {
-        //             this.request.post('/tg-gateway/tg-admin/login',
-        //                 this.ruleForm
-        //             ).then((res) => {
-        //                 if (res.code === '0') {
-        //                     this.$message.success('成功！')
-        //                     this.$router.push('/')
-        //                 } else {
-        //                     this.$message.error('失败！')
-        //                 }
-        //             })
-        //         }
-        //     })
+       
+        // 获取后台的img
+     oNcodeImg () {
+            this.axios.request({
+                method: 'get',
+                url: 'tg-gateway/tg-admin/captchaImage',
+            }).then((res) => {
+              
+                this.codeImg = "data:image/gif;base64," + res.data.data.img;
+                this.ruleForm.uuid = res.data.data.uuid
+            });
+        },
+
+        // rundom () {
+        //     //随机生成函数
+        //     this.request
+        //         .get('http://tglmcs.ghzq.com.cn:9501/tg-gateway/tg-admin/captchaImage').then((res => {
+        //             this.uuid=res.data.uuid
+        //         }))
         // }
-    }
-}
+        login (ruleForm) {
+            this.$refs.ruleForm.validate((valid) => {
+                if (valid) {
+                    this.axios({
+                        method: 'post',
+                        url: '/tg-gateway/tg-admin/login',
+                        data: {
+                            username:this.ruleForm.username,
+                            password: this.ruleForm.password,
+                            code: this.ruleForm.code,
+                            uuid: this.ruleForm.uuid,
+                            rememberMe:this.ruleForm.rememberMe
+                        },
+                    }).then((res) =>
+                    {
+                        if (res.data.code ==0) {
+                            this.$message.success(res.data.msg)
+                            localStorage.setItem('Authorization', res.data.data.Authorization);
+                            this.$router.push('/loginLog')
+                            // 将用户token保存到vuex中
+                            // 存入cookie：
+                            Cookies.set('token', res.data.data.Authorization)
+                            Cookies.set('name', this.ruleForm.username)
+                          
+                        } else {
+                            this.$message.error(res.data.msg)
+                          
+                        }
+                    })
+                    
+                }
+            })
+        }
+    },
+   
+};
+
+
+
+
 
 </script>
 <style scoped lang="scss">
@@ -132,11 +175,15 @@ export default {
             float: right;
             width: 110px;
             height: 40px;
-            background: rgb(209, 209, 209);
+            background:#f5f7fa;
             border-top-right-radius: 4px;
             border-bottom-right-radius: 4px;
-            border: solid 1px rgb(197, 196, 196);
-
+            text-align: center;
+            padding-top: 5px;
+        .codeImg{
+            width: 70px;
+            height: 30px;
+        }
         }
 
     }
